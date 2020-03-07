5486657ef18aefd52ec74a384f5b2e61e152c252
sahil3vedi <st9800@gmail.com>
2020-03-07 23:48:26 +0530

    Added Rotating Frames for Phanron

--- review_3/phaneron.py
+++ review_3/phaneron.py
@@ -11,7 +11,7 @@ def chunkCentres(roadMap):
 
 class phaneron():
     
-    def getminIndex(self):
+    def getminIndices(self):
         x1 = self.refTes.posX
         y1 = self.refTes.posY
         distances = []
@@ -20,9 +20,14 @@ class phaneron():
             y2 = each_centre[1]
             distance = dist(x1,y1,x2,y2)
             distances.append(distance)
-        min_dist = min(distances)
-        min_index = distances.index(min_dist)
-        return min_index
+        s = set(distances)
+        min1 = sorted(s)[0]
+        min2 = sorted(s)[1]
+        min3 = sorted(s)[2]
+        min_index1 = distances.index(min1)
+        min_index2 = distances.index(min2)
+        min_index3 = distances.index(min3)
+        return [min_index1,min_index2,min_index3]
         
         
     def __init__(self, roadMap, refTes):
@@ -36,11 +41,85 @@ class phaneron():
         self.chunkCentres = temp[0]
         self.chunkTypes = temp[1]
         self.refTes = refTes
-        self.minIndex = self.getminIndex()
+        self.minIndices = self.getminIndices()
         self.dispImage = loadImage("../assets/teslaTopView.png")
+        self.chunkType = "generic"
+        self.chunkCentre = [0,0]
+        self.nearestChunkTypes = [self.chunkTypes[q] for q in self.minIndices]
+        self.nearestChunkCentres = [self.chunkCentres[r] for r in self.minIndices]
         
+    def dispPhaneron(self,chunkCentre,chunkType):
+        X = chunkCentre[0]
+        Y = chunkCentre[1]
+        pushMatrix()
+        if (chunkType == "UD"):
+            #printing chunk boundary on to the phaneron
+            noFill()
+            stroke(255,240,0)
+            gap = [X-self.refTes.posX,Y-self.refTes.posY]
+            rotate(-self.refTes.angle)
+            translate(gap[0],gap[1])
+            #rect(0,0,self.chunkSide, self.chunkSide)
+            line(0,-self.chunkSide/2,0, self.chunkSide/2)
+        elif (chunkType == "LR"):
+            #printing chunk boundary on to the phaneron
+            noFill()
+            stroke(255,240,0)
+            gap = [X-self.refTes.posX,Y-self.refTes.posY]
+            rotate(-self.refTes.angle)
+            translate(gap[0],gap[1])
+            #rect(0,0,self.chunkSide, self.chunkSide)
+            line(-self.chunkSide/2,0, self.chunkSide/2,0)
+        if (chunkType == "DL"):
+            #printing chunk boundary on to the phaneron
+            noFill()
+            stroke(255,240,0)
+            gap = [X-self.refTes.posX,Y-self.refTes.posY]
+            rotate(-self.refTes.angle)
+            translate(gap[0],gap[1])
+            #rect(0,0,self.chunkSide, self.chunkSide)
+            arc(-self.chunkSide/2, self.chunkSide/2, self.chunkSide, self.chunkSide, -PI/2, 0)
+            arc(0, 0, self.chunkSide, self.chunkSide, -PI/2, 0)
+            arc(-self.chunkSide,self.chunkSide, self.chunkSide, self.chunkSide, -PI/2, 0)
+        if (chunkType == "UR"):
+            #printing chunk boundary on to the phaneron
+            noFill()
+            stroke(255,240,0)
+            gap = [X-self.refTes.posX,Y-self.refTes.posY]
+            rotate(-self.refTes.angle)
+            translate(gap[0],gap[1])
+            #rect(0,0,self.chunkSide, self.chunkSide)
+            arc(self.chunkSide/2, -self.chunkSide/2, self.chunkSide, self.chunkSide, PI/2, PI)
+            arc(0, 0, self.chunkSide, self.chunkSide, PI/2, PI)
+            arc(self.chunkSide,-self.chunkSide, self.chunkSide, self.chunkSide, PI/2, PI)
+        if (chunkType == "DR"):
+            #printing chunk boundary on to the phaneron
+            noFill()
+            stroke(255,240,0)
+            gap = [X-self.refTes.posX,Y-self.refTes.posY]
+            rotate(-self.refTes.angle)
+            translate(gap[0],gap[1])
+            #rect(0,0,self.chunkSide, self.chunkSide)
+            arc(self.chunkSide/2, self.chunkSide/2, self.chunkSide, self.chunkSide, -PI, -PI/2)
+            arc(0, 0, self.chunkSide, self.chunkSide, -PI, -PI/2)
+            arc(self.chunkSide,self.chunkSide, self.chunkSide, self.chunkSide, -PI, -PI/2)
+        if (chunkType == "UL"):
+            #printing chunk boundary on to the phaneron
+            noFill()
+            stroke(255,240,0)
+            gap = [X-self.refTes.posX,Y-self.refTes.posY]
+            rotate(-self.refTes.angle)
+            translate(gap[0],gap[1])
+            #rect(0,0,self.chunkSide, self.chunkSide)
+            arc(-self.chunkSide/2, -self.chunkSide/2, self.chunkSide, self.chunkSide, 0,PI/2)
+            arc(0, 0, self.chunkSide, self.chunkSide, 0, PI/2)
+            arc(-self.chunkSide,-self.chunkSide, self.chunkSide, self.chunkSide, 0, PI/2)
+        popMatrix()s
+
     def update(self):
-        self.minIndex = self.getminIndex()
+        self.minIndices = self.getminIndices()
+        self.nearestChunkTypes = [self.chunkTypes[q] for q in self.minIndices]
+        self.nearestChunkCentres = [self.chunkCentres[r] for r in self.minIndices]
         
     def show(self):
         rectMode(CORNER)
@@ -53,6 +132,16 @@ class phaneron():
         #text(str(self.minIndex), self.posX + self.side/2, self.posY+ self.side/2)
         pushMatrix()
         translate(self.posX + self.side/2, self.posY + self.side/2)
-        image(self.dispImage, 0, 0, 55, 110) #70% reduction in size
+        rectMode(CENTER)
+        image(self.dispImage, 0, 0, 42, 87) #70% reduction in size
+        for i in range(3):
+            a = self.nearestChunkCentres[i]
+            b = self.nearestChunkTypes[i]
+            self.dispPhaneron(a,b)
         popMatrix()
+            
+        
+       
+            
+            
     
